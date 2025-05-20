317e3bd6afb43d35fc76647ad0592adfdee16068
prantik2003 <guinprantik@gmail.com>
2025-05-21 00:54:00 +0530

    commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,7 +2,6 @@ import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/function";
-import { helloWorld } from "@/lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
