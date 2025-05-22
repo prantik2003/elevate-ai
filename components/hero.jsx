3e718aabab54aae7746713c1491d4721723fb8be
Prantik Guin <145896649+prantik2003@users.noreply.github.com>
2025-05-23 00:16:09 +0530

    updates

--- components/hero.jsx
+++ components/hero.jsx
@@ -31,13 +31,13 @@ const HeroSection = () => {
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
           <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
-            Your AI Career Coach for
+            Your AI Guide to Elevating
             <br />
             Professional Success
           </h1>
           <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
-            Advance your career with personalized guidance, interview prep, and
-            AI-powered tools for job success.
+            Accelerate Your Career with Tailored Guidance, Interview Prep, and
+            AI-Driven Tools for Job Success.
           </p>
         </div>
         <div className="flex justify-center space-x-4">
